2abf9af85014bbedeab61bdd6610153cb4ebfae8
seongjikang <java7ang@gmail.com>
2021-12-31 00:05:43 +0900

    MVC 프로젝트 DB 연동 및 테이블 생성하기

---
+++ BookListWithMVC/BookListWithMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookListWithMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
