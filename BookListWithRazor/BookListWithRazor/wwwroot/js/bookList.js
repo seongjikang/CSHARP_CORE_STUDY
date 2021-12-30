46d56b0093e70facc7d65442db540733fc7a4e86
강성지 [java7ang] <java7ang@nexon.co.kr>
2021-12-30 14:10:09 +0900

    MVC 로 수정하기 모듈 작성

--- BookListWithRazor/BookListWithRazor/wwwroot/js/bookList.js
+++ BookListWithRazor/BookListWithRazor/wwwroot/js/bookList.js
@@ -9,7 +9,7 @@ function loadDataTable() {
         "ajax": {
             "url": "/api/book",
             "type": "GET",
-            "datatype":"json"
+            "datatype": "json"
         },
         "columns": [
             { "data": "name", "width": "20%" },
@@ -23,17 +23,44 @@ function loadDataTable() {
                             Edit
                         </a>
                         &nbsp;
-                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'>
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Delete('/api/book?id='+${data})>
                             Delete
                         </a> 
                     </div>    
                     `;
-                }, "width":"40%"
+                }, "width": "40%"
             }
         ],
         "language": {
             "emptyTable": "no data found"
         },
-        "width":"100%"
-    })
-} 
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once delted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMoe: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+
+                }
+            });
+        }
+    });
+}
