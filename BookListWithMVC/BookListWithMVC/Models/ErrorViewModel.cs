7a3d54586aa148502b7e31415f8ab5ce75e3908d
강성지 [java7ang] <java7ang@nexon.co.kr>
2021-12-30 17:12:13 +0900

    생성된 MVC 프로젝트 세팅

---
+++ BookListWithMVC/BookListWithMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookListWithMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
